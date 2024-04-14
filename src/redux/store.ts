a3071a3b93323fdd9047fcfbe72a41787223cb08
blVictorAguilar <blvictoraguilar@gmail.com>
2024-04-14 12:17:16 -0600

    feat: implement data persist

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,13 +1,45 @@
-import {configureStore} from '@reduxjs/toolkit';
+import {combineReducers, configureStore} from '@reduxjs/toolkit';
 import favoritesReducer from './favoritesSlice';
 import {showsReducer} from './showsSlice';
+import {
+  persistReducer,
+  persistStore,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from '@react-native-async-storage/async-storage';
+
+const favoritesPersistConfig = {
+  key: 'favorites',
+  storage,
+};
+
+const showsPersistConfig = {
+  key: 'shows',
+  storage,
+};
+
+const combinedReducers = combineReducers({
+  favorites: persistReducer(favoritesPersistConfig, favoritesReducer),
+  shows: persistReducer(showsPersistConfig, showsReducer),
+});
+
 const store = configureStore({
-  reducer: {
-    favorites: favoritesReducer,
-    shows: showsReducer,
-  },
+  reducer: combinedReducers,
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
+export const persistor = persistStore(store);
+
 export default store;
